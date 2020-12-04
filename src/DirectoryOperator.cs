63e234a3f312321dd49d0f8cd1b61447080199e0
longshilin <583297550@qq.com>
2020-12-04 17:52:23 +0800

    Initial commit

---
+++ src/DirectoryOperator.cs
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------------
+// The MIT License
+// Simple Entity Component System framework https://github.com/longshilin/longshilin.common
+// Copyright (c) 2019-2020 Longshilin <yilong0722@gmail.com>
+// ----------------------------------------------------------------------------
+
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+using System.IO;
+
+namespace Longshilin.Common
+{
+    public class DirectoryOperator
+    {
+        ///
+        public static void DeleteAll(string dirPath)
+        {
+            var di = new DirectoryInfo(dirPath);
+
+            foreach (var file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (var dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+    }
+}
